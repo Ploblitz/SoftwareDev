730ef46110f1378a5f6b22a8b192de795ac4d595
Ploblitz Morris <ploblitzz@gmail.com>
2024-09-19 17:41:17 +0000

    added basic java code one with a print and a second one with a loop to print a certain number of times

---
+++ Welcome1.java
@@ -0,0 +1,10 @@
+// Michael Morris
+// 19/09/2024 
+
+public class Welcome1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
---
+++ Welcome2.java
@@ -0,0 +1,15 @@
+// Michael Morris
+// 19/09/2024 
+
+public class Welcome1
+{
+    public static void main(String[] args)
+    {
+        int b = 5;
+        int c = 1;
+        for(int a = c; a <= b; a++)
+        {
+        System.out.println("Welcome to java programming");
+        }
+    }
+}
